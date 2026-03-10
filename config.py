0b374177bcd4cc0ee135304d65a782997982b88b
denki307 <200189507+denki307@users.noreply.github.com>
2026-03-10 05:40:27 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+
+# Telegram API Details (Get from my.telegram.org)
+API_ID = 12345678  # Unga 8-digit API ID inga podu
+API_HASH = "your_api_hash_here" # Unga API Hash-ah double quotes-kulla podu
+
+# String Session (Userbot login details)
+STRING_SESSION = "your_string_session_here"
+
+# Bot Owner & Support Details
+OWNER_ID = 123456789 # Unga Numeric Telegram ID
+OWNER_USERNAME = "@YourUsername" # Unga Username (@ oda)
+SUPPORT_GROUP = "https://t.me/YourSupportGroup" # Support group link
+CHANNEL_LINK = "https://t.me/YourChannel" # Channel link
+
+# Bot Media
+START_IMG = "https://telegra.ph/file/your_image_link.jpg" # Start image URL
+
+# Logger Support
+LOGGER_GROUP = -1001234567890 # Error logs vara vendiya Group ID (Kandippa '-' oda start aaganum)
+
